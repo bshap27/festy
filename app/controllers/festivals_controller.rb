46dd4257835c83003e1ee1ecbd331b1a71f4e243
bshap27 <rshapiro3@gmail.com>
2015-04-12 18:07:37 -0400

    fest index page table is sortable with functional arrows from bootstrap glyphicons

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,12 +1,30 @@
 class FestivalsController < ApplicationController
+  helper_method :sort_column, :sort_direction
   
   def index
-    @festivals = Festival.order("name ASC")
+    if sort_column == "count" && sort_direction == "desc"
+      @festivals = Festival.all.sort{|f1,f2| f2.artists.count <=> f1.artists.count}
+    elsif sort_column == "count" && sort_direction == "asc"
+      @festivals = Festival.all.sort{|f1,f2| f1.artists.count <=> f2.artists.count}
+    else
+      @festivals = Festival.order(sort_column + " " + sort_direction)
+    end
   end
 
   def show
-    @festivals = Festival.order("name ASC")
+    @festivals = Festival.order("name")
     @festival = Festival.find(params[:id])
-    @artists = @festival.artists.order("sort_order ASC")
+    @artists = @festival.artists.order("sort_order")
   end
+
+  private
+
+  def sort_column
+    %w[name count start_date end_date].include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    if column == "count"
+      direction = column == sort_column && sort_direction == "desc" ? "asc" : "desc"
+    else
+      direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    end
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
 end
